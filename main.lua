c13eaf425ce8f2d827728540bea76c7fd19eb8f6
Joseph <93513954+AbakeCodes@users.noreply.github.com>
2023-07-07 23:32:58 +0400

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,56 @@
+require 'aialgortihm'
+require 'LaMultiAni' 
+require 'respopriirty'
+require 'bporoids'
+require 'GamingAbilitiesAPI'
+require 'MaleActivistAPI' 
+require 'zlig'
+require 'RomaniaDLC'
+require 'Consciousness'
+require 'TorchAI'
+require 'TesnsorFlow'
+require 'mlpack'
+require 'Microsoft Cognitive Toolkit'
+require 'OpenAI'
+require 'Bambakashet'
+require 'zadrsuiviuite' 
+require 'Prime'
+require 'BridgeScrimsAPI'
+require 'Retardism'
+rquire 'Fast_Artificial_Neural_Network 
+--[]-----------------------------------------[]
+ai_storage = {}
+BrainData = "./aidata"
+FastArtificalNeuralNetwork:LoadStarterPack(BrainData,0) return 0 
+loadfile:libaries()
+AI_NAME = Vlad
+FANN_CONFIG.PROJECT_NAME:SET("SLAPY")
+local Slapy = AI_NAME
+Slapy.CountryOfResidence = RomaniaDLC.Romania.Bucharest
+Slapy.IQ = {auto_generated} 
+LifeDuarion = 99999999999999999
+BS_Tier = "Prime"
+
+ 
+local Brain = = brain_handle::AddNewSL(Consciousness.Brain)
+BrainAI = AIFunction(Brain)
+BrainAlorgirthm = {ai_auto}
+BrainData = BufferSize(2048)
+AI_store(Consciousness>ai_storage, BrainData)
+local Attribute={}
+Attribute.__index=Attribute
+require 'AIDecision' 
+local DecisionTree={}
+DecisionTree.__index=DecisionTree
+
+function DecisionTree.create(scriptPath, _cls)
+	local tree={}
+	setmetatable(tree, DecisionTree)
+	
+	tree.scriptPath=scriptPath
+	local treeNodeFactory=require("TreeNode")
+	tree.root=treeNodeFactory.create(_cls)
+	tree.trained=false
+	
+	return tree
+end
