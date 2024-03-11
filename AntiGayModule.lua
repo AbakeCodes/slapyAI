8c33cc38d2cef2a758257555030bdbd7f7c6c933
the <93513954+AbakeCodes@users.noreply.github.com>
2024-03-11 20:36:57 +0400

    Create AntiGayModule.lua

---
+++ AntiGayModule.lua
@@ -0,0 +1,15 @@
+-- THIS CODE ENSURES SLAPY BUG WHERE IT  DOES NOT SUPPORT FAGGOTS 
+
+_G "main.lua" 
+
+local GayPeople = WorldDLC.HumanPackage.MentallyILLSection(" i identify ahh people")
+
+
+function hateGayPeople() 
+AIStorage:AddNewCheck("I hate faggots", 0) 
+return 0
+Slapy.LocalFunction(AI_MOOD = "ANGRY")
+end 
+
+
+  
