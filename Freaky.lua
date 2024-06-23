65eb9fc625e461c7590ee4337862abaf8e1c61dd
the <93513954+AbakeCodes@users.noreply.github.com>
2024-06-23 21:39:15 +0400

    Update Freaky.lua

--- Freaky.lua
+++ Freaky.lua
@@ -1,4 +1,10 @@
 local Storage = require('DataStorageModule')
 local main = require('main")
 
+SlapyFreaky = false 
+AddNewData.Mood:Storage("FREAKY")
 
+  function actFreaky()
+if aiCommunication_recieved("Lets get freaky") then SlapyFreaky = true
+      if SlapyFreaky = true then do 
+          
