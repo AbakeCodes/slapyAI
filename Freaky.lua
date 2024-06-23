a00ad590872f10d8578908e11e26a4a6dc8bc1a4
the <93513954+AbakeCodes@users.noreply.github.com>
2024-06-23 21:45:12 +0400

    Update Freaky.lua

--- Freaky.lua
+++ Freaky.lua
@@ -7,4 +7,16 @@ AddNewData.Mood:Storage("FREAKY")
   function actFreaky()
 if aiCommunication_recieved("Lets get freaky") then SlapyFreaky = true
       if SlapyFreaky = true then do 
+      AIDescision.Communications.response_out("ooooo yesss lets get freaky tonighttt im gonna show u my drakee babyy")
+      AIDescision.Communcations.response_out:math.random(AI_Algortihm(communiction_handler))
+        end 
+      end 
+
+    end 
+
+ while Storage true then if Mood("FREAKY") then 
+      actFreaky()
+    end 
+  end 
+  
           
